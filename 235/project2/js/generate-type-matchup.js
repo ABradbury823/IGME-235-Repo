b86ef5516d0efaf81e382913a708c09165d79424
amb7316 <56976617+amb7316@users.noreply.github.com>
2021-04-21 19:39:07 -0400

    Full functionality for type match-ups

--- 235/project2/js/generate-type-matchup.js
+++ 235/project2/js/generate-type-matchup.js
@@ -1,5 +1,5 @@
 window.addEventListener( "load",(e) => {document.querySelector("#generate").onclick = generateButtonClicked});
-window.addEventListener("load", (e) => {document.querySelector("#type1").onchange = noSelectingDoubleType});
+window.addEventListener("load", (e) => {document.querySelector("#type1").onchange = dropDownSelectionChanged});
 
 let type1 = "";
 let type2 = "";
@@ -65,7 +65,42 @@ function getMatchupData(url){
 //PURPOSE: sorts type matchup data into proper division
 //ARGUMENT: XMLHttpRequest containing type matchup data
 function dataMatchupLoaded(e){
+    let xhr = e.target;
+    //console.log(xhr.responseText);
 
+    let obj = JSON.parse(xhr.responseText);
+
+    let damageRelations = obj.damage_relations;
+    //console.log(damageRelations);   //2xdmg_from, 2xdmg_to, .5xdmg_from, .5xdmg_to, 0dmg_from, 0dmg_to
+
+    //sort the damage relations so they match the page's layout:
+    //2xdmg_to, .5xdmg_to, 0dmg_to, 2xdmg_from, .5dmg_from, 0dmg_from
+    let reorderedArray = reorderArray(Object.entries(damageRelations));
+    console.log(reorderedArray);
+
+    //sort damage relations into proper div list
+    let lists = document.querySelectorAll("ul");
+
+    //clear lists before adding more entries
+    for(let i = 0; i < lists.length; i++){
+        while(lists[i].firstChild){
+            lists[i].removeChild(lists[i].firstChild);
+        }
+    }
+
+    for(let i = 0; i < lists.length; i++){
+        addArrayToList(reorderedArray[i], lists[i]);
+    }
+    
+    //update status
+    if(type2 == ""){
+        document.querySelector("#status").innerHTML = 
+        `<b>Here are the type matchups for ${capitalizeFirstLetter(type1)} types:</b>`;
+    }
+    else{
+        document.querySelector("#status").innerHTML = 
+        `<b>Here are the type matchups for ${capitalizeFirstLetter(type1)} and ${capitalizeFirstLetter(type2)} types:</b>`;
+    }
 }
 
 //PURPOSE: displays an error message in the console
@@ -74,10 +109,46 @@ function dataMatchupError(e){
     console.log("An occurred while loading type matchups!");
 }
 
+//PURPOSE: sort damage relations into the correct order
+//ARGUMENTS: an array of entries including the damage relations
+//RETURNS: an array containing the damage realtions in the order: 2xdmg_to, .5xdmg_to, 0dmg_to, 2xdmg_from, .5dmg_from, 0dmg_from
+function reorderArray(array){
+    //no need to sort if there is only 1 or 2 points
+    if(array.length < 2){return array;}
+    
+    let newArray = [];
 
-//PURPOSE: prevent the same type being selected twice
+    //place odd indices into front
+    for(let i = 1; i < array.length; i += 2){
+        newArray.push(array[i][1]); //types are second index of entry array
+    }
+    //place even indices into back
+    for(let i = 0; i < array.length; i += 2){
+        newArray.push(array[i][1]);
+    }
+
+    return newArray;
+}
+
+//PURPOSE: add each type to the given damage relation list
+//ARGUMENT: an array of types, list where types should be placed
+function addArrayToList(array, list){
+    //don't add anything if there's nothing in the array
+    if(array.length == 0){return;}
+
+    //add a new li element into the proper list
+    for(let i = 0; i < array.length; i++){
+        let type = array[i].name;
+        let newLi = document.createElement("li");
+        newLi.innerText = capitalizeFirstLetter(type);
+        list.appendChild(newLi);
+    }
+
+}
+
+//PURPOSE: updates type2 list when type1 dropdown selection is changed
 //ARGUMENTS: --
-function noSelectingDoubleType(){
+function dropDownSelectionChanged(){
     type1 = document.querySelector("#type1").value;
     //console.log(`Type 1: ${type1} (selection changed)`);
 
@@ -85,13 +156,12 @@ function noSelectingDoubleType(){
     //console.log(`Type 2: ${type2} (selection changed)`);
 
     //re-enable all options in type 2 (don't want options to stay disabled!)
-    const numberOfOptions = 19;
     let options = document.querySelectorAll("#type2 option")
-    for(let i = 0; i < numberOfOptions; i++){options[i].disabled = false;}
+    for(let i = 0; i < options.length; i++){options[i].disabled = false;}
 
     //reset type2 if the same type is selected in type 1
     if(type1 == type2){document.querySelector("#type2").selectedIndex = 0;}
 
-    //disable option in type 2
+    //disable option in type 2 if it has already been selected in type 1
     if(type1 != ""){document.querySelector(`#type2 [value=${type1}]`).disabled = true;}
 }
